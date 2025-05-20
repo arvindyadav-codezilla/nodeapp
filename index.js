665a974cf69b9c7b62a6192d3b9ffcd729f9d74d
arvind yadav <arvind.yadav@codezilla.io>
2025-05-20 13:55:13 +0530

    Add basic Jenkinsfile again 5

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ dd
  *         description: A greeting message
  */
 app.get("/hello", (req, res) => {
-  res.json({ message: "Hello, Swagger!   " });
+  res.json({ message: "Hello, Swagger! f  " });
 });
 
 /**
